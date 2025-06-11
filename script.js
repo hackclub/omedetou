238fb9543057e51bf03c5c691b3ab78f1c98325d
sporeball <sporeballdev@gmail.com>
2025-06-11 11:53:57 -0400

    csv
    getting this out of the way because it's hard

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // const editor = kell('editor', document.getElementById('editor_container'));
 
+let csv_name;
+let csv_data;
+
 async function get_text (url) {
   const response = await fetch(url, { cache: 'no-cache' });
   let text;
@@ -14,6 +17,20 @@ get_text('./print_style.css').then(text => {
   print_style = text;
 })
 
+function read_csv (s) {
+  const intermediate = CSV.parse(s);
+  let columns = intermediate.shift();
+  const result = intermediate.map((row, i) => {
+    let obj = {};
+    for (let c = 0; c < columns.length; c++) {
+      const column = columns[c];
+      obj[column] = row[c];
+    }
+    return obj;
+  });
+  return result;
+}
+
 function print_out () {
   let print_window = window.open('','','width=800,height=600');
   print_window.document.title = 'Print';
@@ -27,6 +44,21 @@ function print_out () {
   print_window.focus();
   print_window.print();
   print_window.close();
+}
+
+document.getElementById('csv_file_input').addEventListener('change', e => {
+  const reader = new FileReader();
+  reader.onload = function (e2) {
+    csv_name = e.target.files[0].name;
+    csv_data = read_csv(e2.target.result);
+    // ui
+    document.getElementById('csv').textContent = `csv: ${csv_name}`;
+  }
+  reader.readAsText(e.target.files[0]);
+}, false);
+
+document.getElementById('upload_csv').onclick = function () {
+  document.getElementById('csv_file_input').click();
 }
 
 document.getElementById('print').onclick = function () {
